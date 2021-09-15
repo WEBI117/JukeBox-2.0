16a0984d079cbe04a85365f118bd244dc5daaafe
Wahid <wejaz@ssidecisions.com>
2021-09-15 23:38:08 +0500

    Basic Spotify player integration.

--- WebApp/ReactFrontEnd/src/App.js
+++ WebApp/ReactFrontEnd/src/App.js
@@ -1,13 +1,60 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Testcomponentfun from './components/testcomponentfunctional/testcomponentfunction'
 import Testcomponentclass from './components/testcomponentclass/testcomponentclass'
+import SpotifyPlayerComponent from './components/SpotifyPlayerComponent/spotifyPlayer';
 
+// Helper Functions.
+
+// TODO: Initiliza Spotify SDK script programmatically...
 function App() {
+  const [player, setPlayer] = useState(null);
+  const [token, setToken] = useState('BQDe_slylH6XrlAF7FAhRhjhvO9PKQz3bGKBIhh1WAb667QXD8UVofIp-TK5RVVyNIk4kYhQPihDUUD_Nx0ybyxzWjNw9CjY4i2_rfua85NUidba7iUp2SzA2mPY8gpo8BUesCrg_DmMPt0gRnrPUDsgSFfWeamqENquShtTI9UOKbj4UKar2eE');
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    document.body.appendChild(script)
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: 'Web Playback SDK',
+        getOAuthToken: cb => { cb(token); },
+        volume: 0.5
+      });
+
+      setPlayer(player);
+
+      // Ready
+      player.on('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
+
+      // Not Ready
+      player.on('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
+
+      player.on('initialization_error', ({ message }) => {
+        console.error(message);
+      });
+
+      player.on('authentication_error', ({ message }) => {
+        console.error(message);
+      });
+
+      player.on('account_error', ({ message }) => {
+        console.error(message);
+      });
+
+      player.connect();
+    }
+  }, [])
+
+
   return (
     <div className="App">
-      <Testcomponentfun message="yo"/>
-      <Testcomponentclass componenttype="class"/>
-      <p> Hello world </p>
+      <Testcomponentfun message='hello'/>
     </div>
   );
 }
